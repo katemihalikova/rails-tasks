5a310461aa6043b7d9d920d47bf2e0e557b616ee
Kate Miháliková <kate@katemihalikova.cz>
2019-12-06 05:45:33 +0100

    Enable bullets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,11 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
